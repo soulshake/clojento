c20884c98a47feacb1ef35e960246f7e9b7210cb
Jean-Luc Geering <jlgeering.12@gmail.com>
2012-11-03 09:06:19 +0100

    new noir website

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-clojento
-========
+# clojento
+
+A website written in noir. 
+
+## Usage
+
+```bash
+lein deps
+lein run
+```
+
+## License
+
+Copyright (C) 2011 Jean-Luc Geering
+
+Distributed under the Eclipse Public License, the same as Clojure.
+
---
+++ src/clojento/views/welcome.clj
@@ -0,0 +1,9 @@
+(ns clojento.views.welcome
+  (:require [clojento.views.common :as common]
+            [noir.content.getting-started])
+  (:use [noir.core :only [defpage]]
+        [hiccup.core :only [html]]))
+
+(defpage "/welcome" []
+         (common/layout
+           [:p "Welcome to clojento"]))
