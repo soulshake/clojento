0b41af113d96f32bcdf847584a7c73f2a95f123a
Jean-Luc Geering <jlgeering.12@gmail.com>
2012-11-07 23:29:59 +0100

    product table

--- src/clojento/views/products.clj
+++ src/clojento/views/products.clj
@@ -8,9 +8,34 @@
 	(common/layout
 	[:h1 "List All Products"]
 	[:hr]
-	[:div [:a {:href (url-for show {:id 1})} "product 1"]]
-	[:div [:a {:href (url-for show {:id 2})} "product 2"]]
-	[:div [:a {:href (url-for show {:id 3})} "product 3"]]))
+	[:table
+		[:thead [:tr
+			[:th "id"]
+			[:th "sku"]
+			[:th "name"]
+			[:th "type"]
+		]]
+		[:tbody
+			[:tr
+				[:td "1"]
+				[:td [:a {:href (url-for show {:id 1})} "123.4567.8.9"]]
+				[:td [:a {:href (url-for show {:id 1})} "product 1"]]
+				[:td "configurable"]
+			]
+			[:tr
+				[:td "2"]
+				[:td [:a {:href (url-for show {:id 2})} "123.1234.8.9"]]
+				[:td [:a {:href (url-for show {:id 2})} "product 2"]]
+				[:td "bundle"]
+			]
+			[:tr
+				[:td "3"]
+				[:td [:a {:href (url-for show {:id 3})} "123.1111.8.9"]]
+				[:td [:a {:href (url-for show {:id 3})} "product 3"]]
+				[:td "simple"]
+			]
+		]
+	]))
 
 (defpage show "/product/:id" {id :id}
 	(common/layout
