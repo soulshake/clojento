2557c5d129621e036a3da9275baa456425a832fb
Jean-Luc Geering <jeanluc.geering@ufirstgroup.com>
2013-09-03 10:04:11 +0200

    updated korma lib

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
                            [noir "1.2.2"]
-                           [korma "0.3.0-beta9"]
+                           [korma "0.3.0-RC5"]
                            [mysql/mysql-connector-java "5.1.6"]]
             :main clojento.server)
 
