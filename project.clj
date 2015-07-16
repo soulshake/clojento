a8988db708cf5ad8d031d4f97276b122907ee25a
Jean-Luc Geering <jlgeering.13@gmail.com>
2015-07-16 07:40:46 +0200

    logging

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/java.jdbc "0.3.7"]
+                 [com.taoensso/timbre "4.0.2"]
                  [mysql/mysql-connector-java "5.1.35"]
-                 [yesql "0.4.0"]])
+                 [yesql "0.4.0"]]
+  :main clojento.magento.db)
