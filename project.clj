1c298d34a78bf4ebb8b5f09b2481c0cd6be334cd
Jean-Luc Geering <jlgeering.13@gmail.com>
2015-07-16 07:29:46 +0200

    updated project

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject clojento "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
-            :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.2"]
-                           [korma "0.3.0-RC5"]
-                           [mysql/mysql-connector-java "5.1.6"]]
-            :main clojento.server)
-
+(defproject clojento "0.2.0-SNAPSHOT"
+  :description "Clojure Magento Tools"
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/java.jdbc "0.3.7"]
+                 [mysql/mysql-connector-java "5.1.35"]
+                 [yesql "0.4.0"]])
