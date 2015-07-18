dd6894aaf0a75fb0a0641cc96754ebf4434e97af
Jean-Luc Geering <jlgeering.13@gmail.com>
2015-07-18 09:03:03 +0200

    see https://github.com/stuartsierra/reloaded

---
+++ dev/user.clj
@@ -0,0 +1,39 @@
+(ns user
+  "Tools for interactive development with the REPL. This file should
+  not be included in a production build of the application."
+  ;(:require
+  ; [clojure.java.io :as io]
+  ; [clojure.java.javadoc :refer [javadoc]]
+  ; [clojure.pprint :refer [pprint]]
+  ; [clojure.reflect :refer [reflect]]
+  ; [clojure.repl :refer [apropos dir doc find-doc pst source]]
+  ; [clojure.set :as set]
+  ; [clojure.string :as str]
+  ; [clojure.test :as test]
+  ;)
+
+  (:require [com.stuartsierra.component :as component]
+            [clojure.tools.namespace.repl :refer [refresh refresh-all]]
+            [clojento.core :as app]))
+
+(def system nil)
+
+(defn init []
+  (alter-var-root #'system
+    (constantly (app/example-system {:host "dbhost.com" :port 123}))))
+
+(defn start []
+  (alter-var-root #'system component/start))
+
+(defn stop []
+  (alter-var-root #'system
+    (fn [s] (when s (component/stop s)))))
+
+(defn go []
+  (init)
+  (start)
+  :ready)
+
+(defn reset []
+  (stop)
+  (refresh :after 'user/go))
--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/java.jdbc "0.3.7"]
-                 [org.clojure/tools.namespace "0.2.11"]
                  [com.stuartsierra/component "0.2.3"]
                  [com.taoensso/timbre "4.0.2"]
                  [mysql/mysql-connector-java "5.1.35"]
-                 [yesql "0.4.0"]])
+                 [yesql "0.4.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]]
+                   :source-paths ["dev"]}})
